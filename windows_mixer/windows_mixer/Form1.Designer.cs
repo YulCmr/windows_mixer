63906a190a87bd67c9b35c57e19676512b398502
YulCmr <61750323+YulCmr@users.noreply.github.com>
2020-10-29 16:59:07 +0100

    quick update

--- windows_mixer/windows_mixer/Form1.Designer.cs
+++ windows_mixer/windows_mixer/Form1.Designer.cs
@@ -31,6 +31,18 @@
             this.components = new System.ComponentModel.Container();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.volumeMeter1 = new NAudio.Gui.VolumeMeter();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.volumeMeter2 = new NAudio.Gui.VolumeMeter();
+            this.volumeMeter3 = new NAudio.Gui.VolumeMeter();
+            this.volumeMeter4 = new NAudio.Gui.VolumeMeter();
+            this.volumeMeter5 = new NAudio.Gui.VolumeMeter();
+            this.volumeMeter6 = new NAudio.Gui.VolumeMeter();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // timer1
@@ -42,23 +54,148 @@
             // volumeMeter1
             // 
             this.volumeMeter1.Amplitude = 0F;
-            this.volumeMeter1.Location = new System.Drawing.Point(35, 59);
+            this.volumeMeter1.Location = new System.Drawing.Point(65, 191);
             this.volumeMeter1.MaxDb = 18F;
             this.volumeMeter1.MinDb = -60F;
             this.volumeMeter1.Name = "volumeMeter1";
-            this.volumeMeter1.Size = new System.Drawing.Size(34, 337);
+            this.volumeMeter1.Size = new System.Drawing.Size(27, 233);
             this.volumeMeter1.TabIndex = 0;
             this.volumeMeter1.Text = "volumeMeter1";
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // volumeMeter2
+            // 
+            this.volumeMeter2.Amplitude = 0F;
+            this.volumeMeter2.Location = new System.Drawing.Point(173, 191);
+            this.volumeMeter2.MaxDb = 18F;
+            this.volumeMeter2.MinDb = -60F;
+            this.volumeMeter2.Name = "volumeMeter2";
+            this.volumeMeter2.Size = new System.Drawing.Size(27, 233);
+            this.volumeMeter2.TabIndex = 1;
+            this.volumeMeter2.Text = "volumeMeter2";
+            // 
+            // volumeMeter3
+            // 
+            this.volumeMeter3.Amplitude = 0F;
+            this.volumeMeter3.Location = new System.Drawing.Point(284, 191);
+            this.volumeMeter3.MaxDb = 18F;
+            this.volumeMeter3.MinDb = -60F;
+            this.volumeMeter3.Name = "volumeMeter3";
+            this.volumeMeter3.Size = new System.Drawing.Size(27, 233);
+            this.volumeMeter3.TabIndex = 2;
+            this.volumeMeter3.Text = "volumeMeter3";
+            // 
+            // volumeMeter4
+            // 
+            this.volumeMeter4.Amplitude = 0F;
+            this.volumeMeter4.Location = new System.Drawing.Point(391, 191);
+            this.volumeMeter4.MaxDb = 18F;
+            this.volumeMeter4.MinDb = -60F;
+            this.volumeMeter4.Name = "volumeMeter4";
+            this.volumeMeter4.Size = new System.Drawing.Size(27, 233);
+            this.volumeMeter4.TabIndex = 3;
+            this.volumeMeter4.Text = "volumeMeter4";
+            // 
+            // volumeMeter5
+            // 
+            this.volumeMeter5.Amplitude = 0F;
+            this.volumeMeter5.Location = new System.Drawing.Point(496, 191);
+            this.volumeMeter5.MaxDb = 18F;
+            this.volumeMeter5.MinDb = -60F;
+            this.volumeMeter5.Name = "volumeMeter5";
+            this.volumeMeter5.Size = new System.Drawing.Size(27, 233);
+            this.volumeMeter5.TabIndex = 4;
+            this.volumeMeter5.Text = "volumeMeter5";
+            // 
+            // volumeMeter6
+            // 
+            this.volumeMeter6.Amplitude = 0F;
+            this.volumeMeter6.Location = new System.Drawing.Point(610, 191);
+            this.volumeMeter6.MaxDb = 18F;
+            this.volumeMeter6.MinDb = -60F;
+            this.volumeMeter6.Name = "volumeMeter6";
+            this.volumeMeter6.Size = new System.Drawing.Size(27, 233);
+            this.volumeMeter6.TabIndex = 5;
+            this.volumeMeter6.Text = "volumeMeter6";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(62, 155);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(170, 155);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(281, 155);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(388, 155);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "label4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(493, 155);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "label5";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(607, 155);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "label6";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.volumeMeter6);
+            this.Controls.Add(this.volumeMeter5);
+            this.Controls.Add(this.volumeMeter4);
+            this.Controls.Add(this.volumeMeter3);
+            this.Controls.Add(this.volumeMeter2);
             this.Controls.Add(this.volumeMeter1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -66,6 +203,18 @@
 
         private System.Windows.Forms.Timer timer1;
         private NAudio.Gui.VolumeMeter volumeMeter1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private NAudio.Gui.VolumeMeter volumeMeter2;
+        private NAudio.Gui.VolumeMeter volumeMeter3;
+        private NAudio.Gui.VolumeMeter volumeMeter4;
+        private NAudio.Gui.VolumeMeter volumeMeter5;
+        private NAudio.Gui.VolumeMeter volumeMeter6;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
     }
 }
 
--- windows_mixer/windows_mixer/Form1.cs
+++ windows_mixer/windows_mixer/Form1.cs
@@ -9,17 +9,38 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Runtime.InteropServices;
 using NAudio.CoreAudioApi;
-using NAudio.Wave;
+//using NAudio.Wave;
+using CSCore.CoreAudioAPI;
+using Role = CSCore.CoreAudioAPI.Role;
+using DataFlow = CSCore.CoreAudioAPI.DataFlow;
+using AudioMeterInformation = CSCore.CoreAudioAPI.AudioMeterInformation;
 
 namespace windows_mixer
 {
     public partial class Form1 : Form
     {
+        public List<Label> labels = new List<Label>();
+        public List<NAudio.Gui.VolumeMeter> volumeMeters = new List<NAudio.Gui.VolumeMeter>();
+
         public Form1()
         {
             InitializeComponent();
 
-            MMDeviceEnumerator devEnum = new MMDeviceEnumerator();
+            labels.Add(label1);
+            labels.Add(label2);
+            labels.Add(label3);
+            labels.Add(label4);
+            labels.Add(label5);
+            labels.Add(label6);
+
+            volumeMeters.Add(volumeMeter1);
+            volumeMeters.Add(volumeMeter2);
+            volumeMeters.Add(volumeMeter3);
+            volumeMeters.Add(volumeMeter4);
+            volumeMeters.Add(volumeMeter5);
+            volumeMeters.Add(volumeMeter6);
+
+            /*MMDeviceEnumerator devEnum = new MMDeviceEnumerator();
             MMDevice defaultDevice = devEnum.GetDefaultAudioEndpoint(DataFlow.Render, Role.Multimedia);
 
             Console.WriteLine("Render");
@@ -31,21 +52,87 @@ namespace windows_mixer
                 Console.WriteLine(devEnum.EnumerateAudioEndPoints(DataFlow.Capture, DeviceState.Active)[i]);
 
             //clean up
-            devEnum.Dispose();
+            devEnum.Dispose();*/
+
+            backgroundWorker1.RunWorkerAsync();
 
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            MMDeviceEnumerator devEnum = new MMDeviceEnumerator();
+            /*MMDeviceEnumerator devEnum = new MMDeviceEnumerator();
             MMDevice defaultDevice = devEnum.GetDefaultAudioEndpoint(DataFlow.Render, Role.Multimedia);
             //string currVolume = "MasterPeakVolume : " + defaultDevice.AudioMeterInformation.MasterPeakValue.ToString();
             //Console.WriteLine(currVolume);
             volumeMeter1.Amplitude = defaultDevice.AudioMeterInformation.MasterPeakValue;
 
             //clean up
-            devEnum.Dispose();
+            devEnum.Dispose();*/
+        }
+
+        private static AudioSessionManager2 GetDefaultAudioSessionManager2(DataFlow dataFlow)
+        {
+            using (var enumerator = new CSCore.CoreAudioAPI.MMDeviceEnumerator())
+            {
+                using (var device = enumerator.GetDefaultAudioEndpoint(dataFlow, Role.Multimedia))
+                {
+                    Console.WriteLine("DefaultDevice: " + device.FriendlyName);
+                    var sessionManager = AudioSessionManager2.FromMMDevice(device);
+                    return sessionManager;
+                }
+            }
+        }
+        
+
+        private void set_label(int i, string app_name)
+        {
+            BeginInvoke(new EventHandler(delegate
+            {
+                labels[i].Text = app_name;
+            }));
+        }
+
+        private void set_peak(int i, int peak)
+        {
+            BeginInvoke(new EventHandler(delegate
+            {
+                volumeMeters[i].Amplitude = peak;
+            }));
+        }
+
+        
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            int i, peak;
+            string pName;
+            while (true)
+            {
+                i = 0;
+                using (var sessionManager = GetDefaultAudioSessionManager2(DataFlow.Render))
+                {
+                    using (var sessionEnumerator = sessionManager.GetSessionEnumerator())
+                    {
+                        foreach (var session in sessionEnumerator)
+                        {
+                            using (var session2 = session.QueryInterface<AudioSessionControl2>())
+                            using (var audioMeterInformation = session.QueryInterface<AudioMeterInformation>())
+                            {
+                                peak = (int)audioMeterInformation.GetPeakValue()*100;
+                                pName = session2.Process.ProcessName;
+                                set_label(i, pName);
+                                set_peak(i, peak);
+                                //Console.WriteLine(session2.Process.MainWindowTitle);
+                                //Console.WriteLine(session2.Process.ProcessName);
+                                Console.WriteLine(audioMeterInformation.GetPeakValue()*100);
+                                if (i < 6) i++;
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
 }
+
+
 
